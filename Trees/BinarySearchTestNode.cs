776c8f0cb4f5263d9288584955f24a4c6cc529bc
František Bereň <birko@live.com>
2026-03-12 15:49:45 +0100

    Add initial test project and implement AVL and Binary Search Tree tests

---
+++ Trees/BinarySearchTestNode.cs
@@ -0,0 +1,24 @@
+using Birko.Structures.Trees;
+using System;
+
+namespace Birko.Structures.Tests.Trees
+{
+    internal class BinarySearchTestNode : BinarySearchNode
+    {
+        public int Data { get; set; }
+
+        public override int CompareTo(Node other)
+        {
+            if (other is not BinarySearchTestNode testNode)
+            {
+                throw new ArgumentException("Argument is not a instance of BinarySearchTestNode");
+            }
+            return Data.CompareTo(testNode.Data);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ Trees/BinarySearchTreeTests.cs
@@ -0,0 +1,29 @@
+using Birko.Structures.Extensions.Trees;
+using Birko.Structures.Trees;
+using Xunit;
+
+namespace Birko.Structures.Tests.Trees
+{
+    public class BinarySearchTreeTests
+    {
+        public static Tree testTree = new(new[] {
+            new BinarySearchTestNode() { Data = 9 },
+            new BinarySearchTestNode() { Data = 5 },
+            new BinarySearchTestNode() { Data = 2 },
+            new BinarySearchTestNode() { Data = 3 },
+            new BinarySearchTestNode() { Data = 1 },
+            new BinarySearchTestNode() { Data = 4 },
+            new BinarySearchTestNode() { Data = 8 },
+            new BinarySearchTestNode() { Data = 6 },
+            new BinarySearchTestNode() { Data = 7 },
+        });
+
+        [Fact]
+        public void TestInOrder()
+        {
+            string treePath = string.Join("/", testTree.InOrder());
+            string testPath = string.Join("/", new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+            Assert.Equal(testPath, treePath);
+        }
+    }
+}
